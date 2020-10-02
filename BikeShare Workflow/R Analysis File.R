4f4dc9c3af65605cf1194febd2057a88cb5a57ea
marotjf <71667619+marotjf@users.noreply.github.com>
2020-10-02 16:07:23 -0400

    Finishing the data description and cleaning and refactoring the data.

--- BikeShare Workflow/R Analysis File.R
+++ BikeShare Workflow/R Analysis File.R
@@ -3,28 +3,39 @@ if(require(pacman)==FALSE) install.packages("pacman")
 pacman::p_load(readxl,fpp2,ggplot2,scales,dplyr, skimr, DataExplorer)
 
 
-
-
-
 #Reading in the data
 setwd("M:/ISA 616/ISA616BikeShareAnalysis/BikeShare Workflow")
 bikeshare <- read.csv("Bike Share Data.csv", stringsAsFactors = TRUE)
 
 
 #Data Description Code
-library(skimr)
-skim(bikeshare)
-
-
 
 str(bikeshare)
 
-bikeshare$season <- factor(ebay.data$Competitive, levels=c(0,1), labels=c("Not Competitive", "Competitive"))
+#Splitting date and time column into a separate date column and numeric time column
+
+bikeshare$Date <- sapply(strsplit(as.character(bikeshare$datetime), " "), "[", 1)
+bikeshare$Time <- sapply(strsplit(as.character(bikeshare$datetime), " "), "[", 2)
+bikeshare$Date <- as.Date(bikeshare$datetime)
+bikeshare$Time <- as.factor(bikeshare$Time)
+bikeshare$Time <- as.numeric(bikeshare$Time)
+
+#recoding other variables as factors 
+bikeshare$season <- factor(bikeshare$season)
 bikeshare$holiday <- factor(bikeshare$holiday)
 bikeshare$workingday <- factor(bikeshare$workingday)
 bikeshare$weather <- factor(bikeshare$weather)
+
+#Removing original datetime column
+library(dplyr)
+bikeshare<-select(bikeshare, -datetime)
+
+
 str(bikeshare)
 
-ebay.data$Competitive <- factor(ebay.data$Competitive, levels=c(0,1), labels=c("Not Competitive", "Competitive"))
+library(skimr)
+skim(bikeshare)
 
-create_report(df)
+source('data summary.R')
+data.summary(bikeshare)
+)
