c30d19403fe353d0a1341a867d1432d266775917
marotjf <71667619+marotjf@users.noreply.github.com>
2020-10-02 19:00:20 -0400

    Completed multiple regression analysis for predicting the total count of rentals per day, and total count of casual and registered riders per day.

--- BikeShare Workflow/R Analysis File.R
+++ BikeShare Workflow/R Analysis File.R
@@ -51,11 +51,13 @@ bikeshare<-cbind(bikeshare, dum[,-1])
 
 
 dum4<-as.data.frame(model.matrix(~0+bikeshare$weather))
-colnames(dum4)<-c("Clear", "Mist", "Light Snow/Rain", "Heavy Rain/Snow")
+colnames(dum4)<-c("Clear", "Mist", "Light_SnowRain", "Heavy_SnowRain")
 bikeshare<-cbind(bikeshare, dum4[,-1])
 
 bikeshare<-select(bikeshare, -season)
 bikeshare<-select(bikeshare, -weather)
+
+
 str(bikeshare)
 
 #Outlier Detection
@@ -75,4 +77,116 @@ M<-cor(bikeshare[,nums], use="complete.obs")
 corrplot(M, method="circle")
 
 
-#BEGINNNING OF Analysis
+#BEGINNNING OF Analysis - Splitting into Training and Validation -Predicting Full Count
+
+#split into testing and validation
+set.seed(13)
+trainIndex = sample(1:nrow(bikeshare), size = round(0.7*nrow(bikeshare)), replace=FALSE)
+head(trainIndex, 10)
+
+bikeshare.train<-bikeshare[trainIndex, ]
+bikeshare.valid<-bikeshare[-trainIndex, ]
+nrow(bikeshare.train)
+
+
+#For this analysis we are looking at overall count. Therefore we need to take casual and registered out of the data frame for the analysis because count is the sum of these variables
+bikeshare.train<-select(bikeshare.train, -registered)
+bikeshare.train<-select(bikeshare.train, -casual)
+
+bikeshare.valid<-select(bikeshare.valid, -registered)
+bikeshare.valid<-select(bikeshare.valid, -casual)
+
+#???Deseasonalizing DatA???
+
+
+
+#first fitting model with all variables entered. Needed to write it out long ways because cannot include registered and casual when predicting count
+full<-lm(count~.,data=bikeshare.train)
+
+summary(full)
+
+
+#Next using stepwise function to create multiple regression for total Count
+
+null<-lm(bikeshare.train$count~1, data=bikeshare.train)
+lm.step<-step(null, scope=list(lower=null, upper=full), direction="both", trace=0)#trace=0 suppresses output.  Change trace=1 or 2 to get more
+summary(lm.step)
+
+#Evaluate Model
+
+library(forecast)
+p.full<-predict(full, newdata=bikeshare.valid)
+p.step<-predict(lm.step, newdata=bikeshare.valid)
+head(p.full)
+
+
+accuracy(p.step, bikeshare.valid$count)
+
+########################################################
+
+#Analysis for to predict # of registered riders
+
+set.seed(13)
+trainIndex = sample(1:nrow(bikeshare), size = round(0.7*nrow(bikeshare)), replace=FALSE)
+head(trainIndex, 10)
+
+bikeshare.train<-bikeshare[trainIndex, ]
+bikeshare.valid<-bikeshare[-trainIndex, ]
+nrow(bikeshare.train)
+
+bikeshare.train<-select(bikeshare.train, -count)
+bikeshare.valid<-select(bikeshare.valid, -count)
+
+full<-lm(registered~.,data=bikeshare.train)
+
+summary(full)
+
+null<-lm(bikeshare.train$registered~1, data=bikeshare.train)
+lm.step<-step(null, scope=list(lower=null, upper=full), direction="both", trace=0)#trace=0 suppresses output.  Change trace=1 or 2 to get more
+summary(lm.step)
+
+#Evaluate Model
+
+library(forecast)
+p.full<-predict(full, newdata=bikeshare.valid)
+p.step<-predict(lm.step, newdata=bikeshare.valid)
+head(p.full)
+
+
+accuracy(p.step, bikeshare.valid$registered)
+
+########################################################
+
+#Analysis for to predict # of casual riders
+
+set.seed(13)
+trainIndex = sample(1:nrow(bikeshare), size = round(0.7*nrow(bikeshare)), replace=FALSE)
+head(trainIndex, 10)
+
+bikeshare.train<-bikeshare[trainIndex, ]
+bikeshare.valid<-bikeshare[-trainIndex, ]
+nrow(bikeshare.train)
+
+bikeshare.train<-select(bikeshare.train, -count)
+bikeshare.valid<-select(bikeshare.valid, -count)
+
+full<-lm(casual~.,data=bikeshare.train)
+
+summary(full)
+
+null<-lm(bikeshare.train$casual~1, data=bikeshare.train)
+lm.step<-step(null, scope=list(lower=null, upper=full), direction="both", trace=0)#trace=0 suppresses output.  Change trace=1 or 2 to get more
+summary(lm.step)
+
+#Evaluate Model
+
+library(forecast)
+p.full<-predict(full, newdata=bikeshare.valid)
+p.step<-predict(lm.step, newdata=bikeshare.valid)
+head(p.full)
+
+
+accuracy(p.step, bikeshare.valid$casual)
+
+###############################################################
+
