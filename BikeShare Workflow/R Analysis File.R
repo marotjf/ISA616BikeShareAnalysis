ad3257e7c7292f535b14778579ae2f2ef9e4b9ee
marotjf <71667619+marotjf@users.noreply.github.com>
2020-10-02 17:55:12 -0400

    Completion of Preprocessing step.

--- BikeShare Workflow/R Analysis File.R
+++ BikeShare Workflow/R Analysis File.R
@@ -1,6 +1,6 @@
 ## Manage Packages
 if(require(pacman)==FALSE) install.packages("pacman")
-pacman::p_load(readxl,fpp2,ggplot2,scales,dplyr, skimr, DataExplorer)
+pacman::p_load(readxl,fpp2,ggplot2,scales,dplyr, skimr, DataExplorer,corrplot)
 
 
 #Reading in the data
@@ -16,7 +16,7 @@ str(bikeshare)
 
 bikeshare$Date <- sapply(strsplit(as.character(bikeshare$datetime), " "), "[", 1)
 bikeshare$Time <- sapply(strsplit(as.character(bikeshare$datetime), " "), "[", 2)
-bikeshare$Date <- as.Date(bikeshare$datetime)
+bikeshare$Date <- as.Date(bikeshare$datetime,format='%m/%d/%Y')
 bikeshare$Time <- as.factor(bikeshare$Time)
 bikeshare$Time <- as.numeric(bikeshare$Time)
 
@@ -31,20 +31,48 @@ library(dplyr)
 bikeshare<-select(bikeshare, -datetime)
 
 
-str(bikeshare)
+#str(bikeshare)
 
-library(skimr)
-skim(bikeshare)
 
-source('data summary.R')
-data.summary(bikeshare)
+
+#source('data summary.R')
+#data.summary(bikeshare)
 
 
 
 #PREPROCESSING STEPS
 
 #Creating Dummies for Factors
-dum<-as.data.frame(model.matrix(~0+bikeshare$season))
 
+dum<-as.data.frame(model.matrix(~0+bikeshare$season))
 colnames(dum)<-c("spring", "summer", "fall", "winter")
-head(dum)
+bikeshare<-cbind(bikeshare, dum[,-1])
+#Possibly remove seasonal attribute
+
+
+dum4<-as.data.frame(model.matrix(~0+bikeshare$weather))
+colnames(dum4)<-c("Clear", "Mist", "Light Snow/Rain", "Heavy Rain/Snow")
+bikeshare<-cbind(bikeshare, dum4[,-1])
+
+bikeshare<-select(bikeshare, -season)
+bikeshare<-select(bikeshare, -weather)
+str(bikeshare)
+
+#Outlier Detection
+
+boxplot(bikeshare$count)
+Outliers = boxplot(bikeshare$count)$out
+Outliers
+which(bikeshare$count %in% Outliers)
+
+
+#Plots to look at data
+plot(as.Date(bikeshare$Date), bikeshare$count)
+
+library(corrplot)
+nums<-unlist(lapply(bikeshare, is.numeric))
+M<-cor(bikeshare[,nums], use="complete.obs")
+corrplot(M, method="circle")
+
+
+#BEGINNNING OF Analysis
