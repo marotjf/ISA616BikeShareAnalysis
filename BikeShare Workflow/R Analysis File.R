a1069f8fae1ae3ed75adf9a3019f1d818dca381b
marotjf <71667619+marotjf@users.noreply.github.com>
2020-10-08 10:46:45 -0400

    Final adjustments to written explanations and formatting.

--- BikeShare Workflow/R Analysis File.R
+++ BikeShare Workflow/R Analysis File.R
@@ -6,9 +6,6 @@ pacman::p_load(readxl,fpp2,ggplot2,scales,dplyr, forecast, rsample, rpart, rpart
 setwd("M:/ISA 616/ISA616BikeShareAnalysis/BikeShare Workflow")
 bikeshare <- read.csv("Bike Share Data.csv", stringsAsFactors = TRUE)
 
-
-###############################################################################################
-
 #Data Description Code
 
 str(bikeshare)
@@ -20,7 +17,6 @@ data.summary(bikeshare)
 DT::datatable(head(bikeshare, 10))
 DT::datatable(tail(bikeshare, 10))
 
-#################################################################################
 
 #PREPROCESSING STEPS
 
@@ -58,28 +54,7 @@ bikeshare<-select(bikeshare, -quarter)
 bikeshare<-select(bikeshare, -weather)
 
 
-#Plotting Count over Days
 
-bikeshare$Date <- as.POSIXct.Date(bikeshare$Date) #using this variable just for the plot
-dfplot =data.frame(select(bikeshare,Date, count))
-
-PLOT<-function(x){
-  theme_set(theme_gray(base_size = 14))
-  label=colnames(x)[2]
-  g=ggplot(x, aes(Date, x[,2])) 
-  g=g+ geom_line()
-  g = g + scale_x_datetime(name = "Date", 
-                           breaks = date_breaks("70 days"),
-                           labels = date_format(format = "%b %d"),
-                           expand = c(0,0))
-  g=g+xlab("") + ylab(paste0("Count of Rentals",label))
-  print(g)
-}
-
-PLOT(dfplot)
-
-bikeshare<-select(bikeshare, -Time)
-bikeshare<-select(bikeshare, -Date)
 
 str(bikeshare)
 
@@ -91,8 +66,7 @@ Outliers
 which(bikeshare$count %in% Outliers)
 
 
-#Plots to look at data
-plot(as.Date(bikeshare$Date), bikeshare$count)
+
 
 library(corrplot)
 nums<-unlist(lapply(bikeshare, is.numeric))
@@ -120,7 +94,19 @@ bikeshare.train<-select(bikeshare.train, -casual)
 bikeshare.valid<-select(bikeshare.valid, -registered)
 bikeshare.valid<-select(bikeshare.valid, -casual)
 
+#Plotting Count over Days
 
+plot(as.Date(bikeshare$Date), bikeshare$count)
+bikeshare.train<-select(bikeshare.train, -Time)
+bikeshare.train<-select(bikeshare.train, -Date)
+bikeshare.train<-select(bikeshare.train, -Summer)
+bikeshare.train<-select(bikeshare.train, -Fall)
+bikeshare.train<-select(bikeshare.train, -Winter)
+bikeshare.valid<-select(bikeshare.valid, -Time)
+bikeshare.valid<-select(bikeshare.valid, -Date)
+bikeshare.valid<-select(bikeshare.valid, -Summer)
+bikeshare.valid<-select(bikeshare.valid, -Fall)
+bikeshare.valid<-select(bikeshare.valid, -Winter)
 
 #first fitting model with all variables entered. Needed to write it out long ways because cannot include registered and casual when predicting count
 full<-lm(count~ holiday+ workingday+ temp +atemp + humidity+ windspeed+Clear+Mist+Light_SnowRain+Heavy_SnowRain,data=bikeshare.train)
@@ -144,101 +130,26 @@ head(p.full)
 
 accuracy(p.step, bikeshare.valid$count)
 
-########################################################
-
-#Analysis for to predict # of registered riders
-
-set.seed(13)
-trainIndex = sample(1:nrow(bikeshare), size = round(0.7*nrow(bikeshare)), replace=FALSE)
-head(trainIndex, 10)
-
-bikeshare.train<-bikeshare[trainIndex, ]
-bikeshare.valid<-bikeshare[-trainIndex, ]
-nrow(bikeshare.train)
-
-bikeshare.train<-select(bikeshare.train, -count)
-bikeshare.valid<-select(bikeshare.valid, -count)
-
-full<-lm(registered~.,data=bikeshare.train)
-
-summary(full)
-
-null<-lm(bikeshare.train$registered~1, data=bikeshare.train)
-lm.step<-step(null, scope=list(lower=null, upper=full), direction="both", trace=0)#trace=0 suppresses output.  Change trace=1 or 2 to get more
-summary(lm.step)
-
-#Evaluate Model
-
-library(forecast)
-p.full<-predict(full, newdata=bikeshare.valid)
-p.step<-predict(lm.step, newdata=bikeshare.valid)
-head(p.full)
-
-
-accuracy(p.step, bikeshare.valid$registered)
-
-########################################################
-
-#Analysis for to predict # of casual riders
-
-set.seed(13)
-trainIndex = sample(1:nrow(bikeshare), size = round(0.7*nrow(bikeshare)), replace=FALSE)
-head(trainIndex, 10)
-
-bikeshare.train<-bikeshare[trainIndex, ]
-bikeshare.valid<-bikeshare[-trainIndex, ]
-nrow(bikeshare.train)
-
-bikeshare.train<-select(bikeshare.train, -count)
-bikeshare.valid<-select(bikeshare.valid, -count)
-
-full<-lm(casual~.,data=bikeshare.train)
-
-summary(full)
-
-null<-lm(bikeshare.train$casual~1, data=bikeshare.train)
-lm.step<-step(null, scope=list(lower=null, upper=full), direction="both", trace=0)#trace=0 suppresses output.  Change trace=1 or 2 to get more
-summary(lm.step)
-
-#Evaluate Model
-
-library(forecast)
-p.full<-predict(full, newdata=bikeshare.valid)
-p.step<-predict(lm.step, newdata=bikeshare.valid)
-head(p.full)
-
-
-accuracy(p.step, bikeshare.valid$casual)
-
-###############################################################
 
 #Regression Tree Analysis
 
-#split into testing and validation
 set.seed(13)
-trainIndex = sample(1:nrow(bikeshare), size = round(0.7*nrow(bikeshare)), replace=FALSE)
-head(trainIndex, 10)
+trainIndex1 = sample(1:nrow(bikeshare), size = round(0.7*nrow(bikeshare)), replace=FALSE)
 
-bikeshare.train<-bikeshare[trainIndex, ]
-bikeshare.valid<-bikeshare[-trainIndex, ]
-nrow(bikeshare.train)
+str(bikeshare)
 
+bikeshare.train1<-bikeshare[trainIndex1, ]
+bikeshare.valid1<-bikeshare[-trainIndex1, ]
 
-bikeshare.train<-select(bikeshare.train, -count)
-bikeshare.valid<-select(bikeshare.valid, -count)
+bikeshare.train1<-select(bikeshare.train1, -count)
+bikeshare.valid1<-select(bikeshare.valid1, -count)
+bikeshare.train1<-select(bikeshare.train1, -registered)
+bikeshare.valid1<-select(bikeshare.valid1, -registered)
 
-
-#best days for casual riders to be out, so that you can send them promos to become registered member
-library(rsample)     # data splitting 
-library(dplyr)       # data wrangling
-library(rpart)       # performing regression trees
-library(rpart.plot)  # plotting regression trees
-library(ipred)       # bagging
-library(caret)       # bagging
 
 m1 <- rpart(
-  formula = casual ~ holiday + workingday + temp + humidity + windspeed + summer + fall + winter, 
-  data    = bikeshare.train,
+  formula = casual ~ ., 
+  data    = bikeshare.train1,
   method  = "anova"
 )
 rpart.plot(m1)
